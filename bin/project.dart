ff82db2d3e33046f7b727219fcb0f6b987b90cef
Gabriel Santos <gsantos@imply.com>
2023-07-09 17:09:06 -0300

    feat: implemented a set of tests to compare time performance between binarySearch and native methods

--- bin/project.dart
+++ bin/project.dart
@@ -1,5 +1,38 @@
+import 'dart:math';
+
 import 'package:project/project.dart' as project;
 
+const int listSize = 25000000;
+int desiredNumber = Random().nextInt(25000000);
+
 void main(List<String> arguments) {
-  print('Hello world: ${project.calculate()}!');
+  binarySearch();
+}
+
+void binarySearch() {
+  List<int> intList = List.generate(listSize, (index) => ++index);
+  final Stopwatch stopwatch = Stopwatch();
+
+  stopwatch.start();
+  int? desiredPosition = project.binarySearch(intList, desiredNumber);
+  stopwatch.stop();
+  print('Binary');
+  print(desiredPosition);
+  print(stopwatch.elapsedMilliseconds);
+  stopwatch.reset();
+
+  stopwatch.start();
+  desiredPosition = intList.indexOf(desiredNumber);
+  stopwatch.stop();
+  print('First Where');
+  print(desiredPosition);
+  print(stopwatch.elapsedMilliseconds);
+  stopwatch.reset();
+
+  stopwatch.start();
+  desiredPosition = intList.indexWhere((element) => element == desiredNumber);
+  stopwatch.stop();
+  print('Index Where');
+  print(desiredPosition);
+  print(stopwatch.elapsed.inMilliseconds);
 }
