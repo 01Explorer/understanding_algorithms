b5b72d3a3a21d999efc0f0754391dae27ced2d87
Gabriel Santos <gabrielsantostca@gmail.com>
2023-07-16 21:08:38 -0300

    feat: implemented the logic to get the longestCommonSubstring

--- lib/project.dart
+++ lib/project.dart
@@ -149,3 +149,21 @@ List<List<int>> longestCommonSubsequence(String word1, String word2) {
   }
   return table;
 }
+
+List<List<int>> longestCommonSubstring(String word1, String word2) {
+  final tableWord1 = word1.split('');
+  final tableWord2 = word2.split('');
+  final table = List.generate(
+      tableWord2.length, (index) => List<int>.filled(tableWord1.length, 0));
+
+  for (int i = 0; i < tableWord1.length; i++) {
+    for (int j = 0; j < tableWord2.length; j++) {
+      if (tableWord2[j] == tableWord1[i]) {
+        table[j][i] = table[j - 1][j - 1] + 1;
+      } else {
+        table[j][i] = 0;
+      }
+    }
+  }
+  return table;
+}
